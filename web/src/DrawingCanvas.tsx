a925315fa2b01028a3aeb6299e0ac0bff28c9785
tripathysagar <tripathysagar08@gmail.com>
2023-11-21 20:43:55 +0530

    after adding canvas clearing and download functionality

--- web/src/DrawingCanvas.tsx
+++ web/src/DrawingCanvas.tsx
@@ -51,17 +51,67 @@ const DrawingCanvas: React.FC = () => {
     setIsDrawing(false);
   };
 
+
+  // to clear canvas after clicking  a buttom
+  const canvasCaptureHandler = (_e: React.MouseEvent<HTMLElement>) => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const image = canvas.toDataURL('image/png');
+      console.log(image); 
+      const link = document.createElement('a');
+      link.download = 'canvas_image.png'; 
+      link.href = image; 
+      canvas.appendChild(link);
+      link.click(); 
+      canvas.removeChild(link);
+    }
+  }
+  
+
+  const clearCanvasHandler = (_e: React.MouseEvent<HTMLElement>) => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
+    }
+  }
   return (
-    <canvas
-      ref={canvasRef}
-      onMouseDown={startDrawing}
-      onMouseMove={draw}
-      onMouseUp={stopDrawing}
-      onMouseOut={stopDrawing}
-      width={800}
-      height={600}
-      style={{ border: '1px solid black' }}
-    />
+    <div className='p-5'>
+
+      <span className='pt-10'>
+        <button 
+        onClick={canvasCaptureHandler}
+        type="button" 
+        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          Submit
+        </button>
+
+        <button 
+        onClick={clearCanvasHandler}
+        type="button" 
+        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          Clear
+        </button>
+      </span>
+
+      <span >
+        <canvas
+          ref={canvasRef}
+          onMouseDown={startDrawing}
+          onMouseMove={draw}
+          onMouseUp={stopDrawing}
+          onMouseOut={stopDrawing}
+          width={800}
+          height={600}
+          style={{ border: '1px solid black' }}
+        />
+      </span>
+
+      
+
+    </div>
   );
 };
 
